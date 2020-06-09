45f830ebb7d9dd740c30c6402667c07fee658abd
Charles <cfyang@cmu.edu>
2020-06-09 18:58:14 -0400

    sdfasdfa

--- 1D/point.py
+++ 1D/point.py
@@ -5,27 +5,30 @@ import matplotlib.pyplot as plt
 def H(J,h,d):
     return -2*J*(d**2)-h*d
 
-def F(J,h,T,d):
-    return -2*J*(d**2) -h*d + T*((1+d)*math.log1p(d)+(1-d)*math.log1p(-d))/2-T*math.log(2)
+def S(J,h,d):
+    return -((1+d)*math.log1p(d)+(1-d)*math.log1p(-d))/2+math.log(2)
 
-def FXvT(J):
-    x = np.linspace(-1,1,100)
-    d = x[1:-1]
-    Temp = [0,.5,1,2,4]
+def F(J,h,T,d):
+    if abs(d)==1:
+        return H(J,h,d)
+    else:
+        return H(J,h,d)-T*S(J,h,d)
+
+def FXvT(J, hpj=1):
+    d = np.linspace(-1,1,100)
+    Temp = [0,.5,1,2,3,4,6,10]
     
     vF=np.vectorize(F)
     
-    for T in Temp:
-        L="T="+str(T)
-        Y= vF(J,1,T,d)
-        Y=np.insert(Y,0,H(J,1,-1))
-        Y=np.append(Y,H(J,1,1))
-        plt.plot(x,Y,label=L)
+    for Tpj in Temp:
+        L="J/T="+str(Tpj)
+        Y= vF(J,hpj*abs(J),Tpj*abs(J),d)
+        plt.plot(d,Y,label=L)
 
     plt.ylabel('Free Energy')
     plt.xlabel('Delta')
     
-    title="J="+str(J)
+    title="J="+str(J)+", h/J="+str(hpj)
     plt.title(title)
     plt.legend()
     
