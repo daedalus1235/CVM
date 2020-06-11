33504c04872d10dd20b56150533cca1311b9bac5
Charles <cfyang@cmu.edu>
2020-06-11 14:44:01 -0400

    finished 1D lattice, point approx, regular solution hamiltonian and binary composition

--- 1D/binary/point.py
+++ 1D/binary/point.py
@@ -1,17 +1,17 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+import scipy.optimize as opt
 
 def H(J,h,x):
     return -J*x*(1-x)-h*x
 
 def S(J,h,x):
-    return -x*math.log(x)-(1-x)*math.log(1-x)
+    if abs(x-.5)<.5:
+        return -x*math.log(x)-(1-x)*math.log(1-x)
+    return 0
 
 def F(J,h,T,x):
-    if abs(x-.5)==.5:
-        return H(J,h,x)
     return H(J,h,x)-T*S(J,h,x)
 
 def FvXT(J, hpj=1):
@@ -19,7 +19,7 @@ def FvXT(J, hpj=1):
     Temp = np.linspace(0, 10, 51) 
     
     Mx=[]
-    MT=[]
+    MF=[]
     
 
     vF=np.vectorize(F)
@@ -27,26 +27,29 @@ def FvXT(J, hpj=1):
     show=0
     
     for Tpj in Temp:
-        L="T/J="+str(Tpj)
-        Y= vF(J,hpj*abs(J),Tpj*abs(J),x)
-        if show%5==0:
+        if show%10==0:
+            Y= vF(J,hpj*abs(J),Tpj*abs(J),x)
+            L="T/J="+str(Tpj)
             plt.plot(x,Y,label=L)
-
-        fun = lambda x: F(J,h,T,x)
+        
+        fun = lambda x: F(J,hpj*abs(J),Tpj*abs(J),x)
         x0=0.5
-        xmin=minimize(fun, x0, method='Nelder-Mead', tol=1e-9)
+        xmin=opt.fminbound(fun,0,1, xtol=1e-9)
+        
+        x0=xmin
+        
+        Mx.append(x0)
+        MF.append(fun(x0))
         
         show+=1
+    
+    plt.plot(Mx, MF,Label='min Free', color='k')
 
     plt.ylabel('Free Energy')
     plt.xlabel('Delta')
     
     title="J="+str(J)+", h/J="+str(hpj)
     plt.title(title)
-    plt.legend(loc='center left', bbox_to_anchor=(0.8,0.5))
+    plt.legend(loc='center left', bbox_to_anchor=(0.9,0.5))
     
     plt.show()
-
-def Min():
-    J,h,T=1,1,1
-    return xmin.x
