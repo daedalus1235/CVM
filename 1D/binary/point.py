2840f6d5c099b769f086c6831b1bdf74ebbd1c38
Charles <cfyang@cmu.edu>
2020-06-11 15:27:35 -0400

    finished minimize function for 1D binary point

--- 1D/binary/point.py
+++ 1D/binary/point.py
@@ -14,7 +14,7 @@ def S(J,h,x):
 def F(J,h,T,x):
     return H(J,h,x)-T*S(J,h,x)
 
-def FvXT(J, hpj=1):
+def FvT(J, hpj=1):
     x = np.linspace(0,1,1000)
     Temp = np.linspace(0, 10, 51) 
     
@@ -33,7 +33,6 @@ def FvXT(J, hpj=1):
             plt.plot(x,Y,label=L)
         
         fun = lambda x: F(J,hpj*abs(J),Tpj*abs(J),x)
-        x0=0.5
         xmin=opt.fminbound(fun,0,1, xtol=1e-9)
         
         x0=xmin
@@ -43,13 +42,31 @@ def FvXT(J, hpj=1):
         
         show+=1
     
-    plt.plot(Mx, MF,Label='min Free', color='k')
+    plt.plot(Mx, MF,Label='min', color='k')
 
     plt.ylabel('Free Energy')
-    plt.xlabel('Delta')
+    plt.xlabel('Composition')
     
     title="J="+str(J)+", h/J="+str(hpj)
     plt.title(title)
     plt.legend(loc='center left', bbox_to_anchor=(0.9,0.5))
     
+    plt.show()
+
+def XvT(J,hpj=1):
+    Temp=np.linspace(0,10,51)
+
+    xmin=[]
+
+    for Tpj in Temp:
+        fun = lambda x: F(J, hpj*abs(J), Tpj*abs(J),x)
+        xmin.append(opt.fminbound(fun, 0, 1, xtol=1e-9))
+
+    plt.plot(Temp, xmin)
+    plt.ylabel("Composition")
+    plt.xlabel("Temperature (T/J)")
+
+    title="J="+str(J)+", h/J="+str(hpj)
+    plt.title(title)
+
     plt.show()
