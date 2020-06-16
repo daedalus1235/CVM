1559a03208ffd52c112ee48561419d06a695d730
Charles <cfyang@cmu.edu>
2020-06-16 19:24:47 -0400

    sfs

--- 1D/binary/bond.py
+++ 1D/binary/bond.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import math
 import scipy.optimize as opt
 
@@ -28,10 +29,11 @@ def Sp(x):
         if x[1]>=0 and x[1]<=x[0] and x[1]<=(1-x[0]): 
             Sy=xlx(x[0]-x[1])+2*xlx(x[1])+xlx(1-x[0]-x[1])
             return Sx-Sy
-    return 1000 
+    return -1 
 
 def F(J,h,x,y,T):
-    return H()-T*S()
+    if T==0: return H(J,h,x,y)
+    return H(J, h, x, y)-T*S(x,y)
 
 def Test():
     rranges = (slice(0,1,0.01), slice(0,0.5,0.01))
@@ -45,4 +47,30 @@ def FvT(J, hpj=1):
 
 
 def XYvT(J, hpj=1):
-    Temp = [0,.25,.5,1,2,3,4,6,10]
+    Temp = np.linspace(0,10,201) 
+    rranges=(slice(0,1,0.01), slice(0,0.5,0.01))
+    
+    con = opt.LinearConstraint([[1,0],[-1,1],[1,1]],[0,-np.inf,-np.inf],[1,0,1])
+    bound = opt.Bounds([0,0],[1,0.5])
+    
+    mX, mY=[],[]
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+       
+    for Tpj in Temp:
+        fun = lambda x: F(J, hpj*abs(J), x[0], x[1], Tpj*abs(J))
+        res=opt.minimize(fun, [0.5,0.25], method='trust-constr', constraints=con, bounds=bound)
+        mX.append(res.x[0])
+        mY.append(res.x[1])
+        working="Calculating T/J:"+str(Tpj)
+        print(working)
+
+    ax.plot(mX, mY, Temp, label='min')
+    ax.set_xlabel('Composition: X')
+    ax.set_ylabel('Composition: Y')
+    ax.set_zlabel('Temperature (T/J)')
+
+    plt.show()
+
