c6b32f48013581d93be840e2669bb69051621ee5
Charles <cfyang@cmu.edu>
2020-06-23 19:16:00 -0400

    adfjaklsfjasl

--- 2D/binary/bond.py
+++ 2D/binary/bond.py
@@ -24,29 +24,9 @@ def S(x, y):
             return Sx-Sy
     return -1 
 
-def Sp(x):
-    Sx,Sy=0,0
-    if abs(x[0]-.5)<=0.5:
-        Sx=xlx(x[0])+xlx(1-x[0])
-        if x[1]>=0 and x[1]<=x[0] and x[1]<=(1-x[0]): 
-            Sy=xlx(x[0]-x[1])+2*xlx(x[1])+xlx(1-x[0]-x[1])
-            return Sx-Sy
-    return -1 
-
 def F(J,h,x,y,T):
     if T==0: return H(J,h,x,y)
     return H(J, h, x, y)-T*S(x,y)
-
-def Test():
-    rranges = (slice(0,1,0.01), slice(0,0.5,0.01))
-    res=opt.brute(Sp, rranges, full_output=True, finish=opt.fmin)
-    return res
-
-def FvT(J, hpj=1):
-    Temp = [0,.25,.5,1,2,3,6,10]
-    for Tpj in Temp:
-        fun = lambda x,y: F(J, hpj*abs(J), x, y, Tpj*abs(J))
-
 
 def XYvT(J, hpj=1):
     Temp = np.linspace(0,10,201) 
@@ -55,20 +35,29 @@ def XYvT(J, hpj=1):
     con = opt.LinearConstraint([[1,0],[-1,1],[1,1]],[0,-np.inf,-np.inf],[1,0,1])
     bound = opt.Bounds([0,0],[1,0.5])
     
-    mX, mY=[],[]
+    Free, mX, mY=[],[],[]
     
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    
-       
     for Tpj in Temp:
         fun = lambda x: F(J, hpj*abs(J), x[0], x[1], Tpj*abs(J))
         res=opt.minimize(fun, [0.5,0.25], method='trust-constr', constraints=con, bounds=bound)
         mX.append(res.x[0])
         mY.append(res.x[1])
+        Free.append(fun(res.x))
         working="Calculating T/J:"+str(Tpj)
         print(working)
-
+    
+    fig = plt.figure(figsize=plt.figaspect(2.))
+    fig.suptitle('2D square lattice, bond approx')
+    
+    #plot min free energy wrt temp
+    ax = fig.add_subplot(2,1,1)
+    ax.set_xlabel('Temperature (T/J)')
+    ax.set_ylabel('Min Free Energy')
+    ax.plot(Temp, Free, label='min free energy')
+    
+    
+    #plot composition wrt temp
+    ax=fig.add_subplot(2,1,2, projection='3d')
     ax.plot(mX, mY, Temp, label='min')
     ax.set_xlabel('Composition: X')
     ax.set_ylabel('Bond Frequency: Y')
---
+++ 2D/binary/square.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+import scipy.optimize as opt
+
+#coefficients for multiplicity
+a=[1,1]         #x
+b=[1,2,1]       #y
+c=[1,4,4,2,4,1] #z
+
+def xlx(x):
+    if x==0: return 0
+    return x*math.log(x)
+
+def H(J,h,z):
+    x = z[1]+2*z[2]+z[3]+3*z[4]+z[5]
+    y = z[1]+z[2]+z[3]+z[4]
+    return -4*J*y-h*x
+
+def S(z):
+    Sx, Sy, Sz = 0,0,0
+
+    for i in range(6):
+        if z[i]<0: return -1
+        Sz-=c[i]*xlx(z[i])
+
+    y = [z[0]+2*z[1]+z[3],
+        z[1]+z[2]+z[3]+z[4],
+        z[2]+2*z[4]+z[5]]
+
+    for i in range(3):
+        Sy-=b[i]*xlx(y[i])
+
+    x = [y[0]+y[1], y[1]+y[2]]
+
+    for i in range(2):
+        Sx-=a[i]*xlx(x[i])
+    
+    return Sx-2*Sy+Sz
+    
+def F(J,h,z,T):
+    if T == 0: return H(J,h,z) #might as well skip entropy calc if possible
+    return H(J,h,z)-T*S(z)
+
+def min(J, hpj=1):
+    h = hpj*abs(J)
+    Temp = np.linspace(0,10,201)
+    
+    #ensure reasonable compositions
+    con = opt.LinearConstraint([[ 1, 3, 2, 1, 1, 0],  # 0 <= x   <= 1
+                                [ 0, 1, 2, 1, 3, 1],  # 0 <= 1-x <= 1
+                                [ 1, 4, 4, 2, 4, 1]], # total proability 1
+                               [0,0,1], [1,1,1])
+    #restrict 0 <= zi <= 1
+    bound = opt.Bounds([0,0,0,0,0,0],[1,1,1,1,1,1])
+    
+    mF, mX, mY = [],[],[] #I need to choose consistent variable names...
+    
+    for Tpj in Temp:
+        status="Calculating T/J:"+str(Tpj)
+        print(status)
+
+        free = lambda z: F(J, h, z, Tpj*abs(J))
+
+        res = opt.minimize(free,                           #minimize free energy
+                [0.125,0.125, 0.125, 0.125, 0.125, 0.125], #starting guess at centre
+                method = 'trust-constr',                   #optimization method
+                constraints = con, bounds = bound)        #set constraints on variables
+        
+        mF.append(free(res.x))
+        mX.append(res.x[0]+3*res.x[1]+2*res.x[2]+res.x[3]+res.x[4])
+        mY.append(res.x[0]+2*res.x[1]+res.x[3])
+
+    fig = plt.figure(figsize=plt.figaspect(2.))
+    fig.suptitle('2D square lattice, square approx')
+
+    #plot min free energy wrt temp
+    ax = fig.add_subplot(2,1,1)
+    ax.set_xlabel('Temperature (T/J)')
+    ax.set_ylabel('Minimum Free Energy')
+    ax.plot(Temp, mF)
+
+    #plot composition wrt temp
+    ax = fig.add_subplot(2,1,2, projection='3d')
+    ax.set_xlabel('Composition: X')
+    ax.set_ylabel('Bond Frequency: Y')
+    ax.set_zlabel('Temperature (T/J)')
+    ax.plot(mX, mY, Temp)
+
+    plt.show()
