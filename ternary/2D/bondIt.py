008c0978e4efca9b1a2386b6052726ebf4f54d70
Charles Yang <cfyang@cmu.edu>
2020-07-29 19:12:59 -0400

    sdfk

--- ternary/2D/bondIt.py
+++ ternary/2D/bondIt.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math
-import scipy.optimize as opt
 
 #Computations are done in terms of y_ij, which is an asymmetric square matrix; the first index denotes the component on the first (even) sublattice , and the second on the second (odd) sublattice
-#the y variable is flattened to form a 1D array to be processed by scipy.optimize
+#helper functions are written to accept square matrices of any size, so can be used for an n-ary composition of arbitrary n>1.
 
-#helper functions
+#helper functions and variables
+z=4
+
 def xlx(x):
     if x<0: return 1
     if x==0: return 0
@@ -20,21 +21,51 @@ def dot(M, N):
             total+= M[i][j]*N[i][j]
     return total
 
-def Xo(y):
-    xo=[]
-    for i in range(3):
-        for j in range(3):
+def norm(y):
+    return dot(y,y)
 
-    return xo
+def transpose(y):
+    yT=[]
+    for j in range(len(y[0])):
+        yTi=[]
+        for i in range(len(y)):
+            yTi.append(y[i][j])
+        yT.append(yTi)
+    return yT
+
+def Yt(y):
+    yT=transpose(y)
+    Yt=[]
+    for i in range(len(y)):
+        Yti=[]
+        for j in range(len(y[i])):
+            Yti.append(y[i][j]+y[j][i])
+        Yt.append(Yti)    
+    return Yt        
 
 def Xe(y):
+    xe=[]
+    for yi in y:
+        xei=0
+        for yij in yi:
+            xei+=yij
+        xe.append(xei)
+    return xe    
+
+def Xo(y):
+    return Xe(transpose(y))
     
-
-
+def Xt(y):
+    xt=[]
+    xe=Xe(y)
+    xo=Xo(y)
+    for i in range(len(xe)):
+        xt.append(xe[i]+xo[i])
+    return xt
 
 #thermodynamic functions
 def H(J,h,y):
-    return -dot(J,y)-dot(h,X(y))
+    return -(z/2)dot(E,Yt(y))
 
 def S(y):
     Sx,Sy=0,0
@@ -50,11 +81,31 @@ def S(y):
 
     return 2*Sy-3*Sx
 
-def F(J,h,y,T):
-    if T==0: return H(J,h,y)
-    return H(J,h,y)-T*S(y)
+def F(E,y,T):
+    if T==0: return H(E,y)
+    return H(E,y)-T*S(y)
 
 
 #minimization
-def min(J=[[]], h=[[]], Trang=[0,4], samp=100):
+def min(E=[[]], Trang=[0,4], samp=100):
+    T=0
 
+    def iterate():
+        yt=y
+        for i in range(len(y)):
+            for j in range(len(y[i])):
+                yt[i][j]=math.exp(-E[i][j]/T) * (Xe(y)*Xo(y))**((z-1)/z)
+
+    
+    
+    temp = np.linspace(Trang[0], Trang[1], samp+1)
+    for i in range(len(temp)):
+        counter = 100
+        T=
+        while(norm(yT)>1):
+            iterate()
+            counter-=1
+            if(counter<1):
+                break
+
+    return 0
