44f423907805dcacbc1d2cf9aa84f43bc102899d
Charles Yang <cfyang@cmu.edu>
2020-07-30 19:26:02 -0400

    iteration when all sites equal, no constraints

--- ternary/2D/bondIt.py
+++ ternary/2D/bondIt.py
@@ -21,8 +21,26 @@ def dot(M, N):
             total+= M[i][j]*N[i][j]
     return total
 
-def norm(y):
-    return dot(y,y)
+def norm(M):
+    return dot(M,M)
+
+def diff(M,N):
+    difference=[]
+    for i in range(len(M)):
+        di=[]
+        for j in range(len(M[i])):
+            di.append(M[i][j]-N[i][j])
+        difference.append(di)
+    return difference
+
+def add(M,N):
+    total=[]
+    for i in range(len(M)):
+        si=[]
+        for j in range(len(M[i])):
+            si.append(M[i][j]+N[i][j])
+        total.append(si)
+    return total
 
 def transpose(y):
     yT=[]
@@ -34,14 +52,8 @@ def transpose(y):
     return yT
 
 def Yt(y):
-    yT=transpose(y)
-    Yt=[]
-    for i in range(len(y)):
-        Yti=[]
-        for j in range(len(y[i])):
-            Yti.append(y[i][j]+y[j][i])
-        Yt.append(Yti)    
-    return Yt        
+    #return add(y, transpose(y))
+    return y
 
 def Xe(y):
     xe=[]
@@ -64,16 +76,15 @@ def Xt(y):
     return xt
 
 #thermodynamic functions
-def H(J,h,y):
-    return -(z/2)dot(E,Yt(y))
+def H(E,y):
+    return -(z/2)*dot(E,Yt(y))
 
 def S(y):
     Sx,Sy=0,0
-    x=X(y)
+    x=Xe(y)#+Xo(y)
 
     for xi in x:
-        for xij in xi:
-            Sx-=xlx(xij)
+        Sx-=xlx(xi)
 
     for yi in y:
         for yij in yi:
@@ -87,25 +98,96 @@ def F(E,y,T):
 
 
 #minimization
-def min(E=[[]], Trang=[0,4], samp=100):
+def min(Eb=[[0,1,1],[1,0,0],[1,0,0]], Trang=[0,4], samp=100):
     T=0
-
     def iterate():
         yt=y
         for i in range(len(y)):
             for j in range(len(y[i])):
-                yt[i][j]=math.exp(-E[i][j]/T) * (Xe(y)*Xo(y))**((z-1)/z)
+                yt[i][j]=math.exp(-Eb[i][j]/T)
+                yt[i][j]*=(Xe(y)[i]*Xo(y)[j])**((z-1)/z)
+        
+        total = 0
+        for yi in yt:
+            for yij in yi:
+                total+=yij
+                
+        for i in range(len(yt)):
+            for j in range(len(yt[i])):
+                y[i][j]=y[i][j]/total
+        
+        print('Updated y:'+str(yt))
 
+        return yt
     
+    delta = (Trang[1]-Trang[0])/samp
+    temp = np.linspace(Trang[0]+delta, Trang[1], samp)
+    tp = np.linspace(Trang[0]+2*delta, Trang[1]-delta, samp-2)
+
+    mY,mF,E,C=[],[],[],[]
     
-    temp = np.linspace(Trang[0], Trang[1], samp+1)
     for i in range(len(temp)):
+        print('Calculating T='+str(temp[i]))
         counter = 100
-        T=
-        while(norm(yT)>1):
-            iterate()
+        T=temp[i]
+        y=[[0.333, 0, 0],[0, 0.333, 0],[0, 0, 0.333]] #starting guess
+        yt=[[0, 0, 0],[0, 0, 0],[0, 0, 0]]            #initialize
+
+        while(norm(diff(y,yt))>1e-9):
+            y=yt
             counter-=1
             if(counter<1):
                 break
+            yt=iterate()
+        
+        mY.append(y)
+        mF.append(F(Eb,y,T))
 
-    return 0
+        if i>1:
+            #calculate E
+            dF=mF[i]-mF[i-2]
+            dF/=(delta*2)
+            E.append(mF[i-1]-temp[i-1]*dF)
+
+            #calculate C
+            ddF=mF[i]-2*mF[i-1]+mF[i-2]
+            ddF/=(delta**2)
+            C.append(-temp[i-1]*ddF)
+
+    fig = plt.figure()
+    fig.suptitle('2D square lattice, bond approx, ternary composition')
+    
+    #todo: composition plot
+
+    ax=fig.add_subplot(2,2,2)
+    ax.set_xlabel('Temperature')
+    ax.set_ylabel('Free Energy')
+    ax.plot(temp,mF)
+
+    ax=fig.add_subplot(2,2,3)
+    ax.set_xlabel('Temperature')
+    ax.set_ylabel('E=F-T*dF/dT')
+    ax.set_ylim(-5,5)
+    ax.plot(tp, E)
+
+    ax=fig.add_subplot(2,2,4)
+    ax.set_xlabel('Temperature')
+    ax.set_ylabel('C=-T*d2F/dT2')
+    ax.set_ylim(-5,5)
+    ax.plot(tp, C)
+
+    #high temp slope should be ln(2)~0.693
+    slope = mF[samp-1]-mF[math.floor(samp*0.75)]
+    slope /= (temp[samp-1]-temp[math.floor(samp*0.75)])
+    print('High temp slope: '+str(slope))
+    #intercept should be ~0
+    intercept=mF[samp-1]-slope*temp[samp-1]
+    print('Intercept: ' + str(intercept))
+
+    plt.show()
+
+
+
+
+
+
